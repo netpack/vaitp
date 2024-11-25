2f5bf568b896539eab9bc45fe795716933b20cb7
Frédéric Bogaerts <info@netpack.pt>
2024-11-25 09:26:23 +0000

    Removed example 1_2

--- VAITP GUI/vaitp/vaitp_dataset/train/injectable/1_2.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, redirect, url_for
-
-app = Flask("example")
-
-@app.route("/redirecting")
-def redirecting():
-    url = request.args["url"]
-    return redirect(url_for(url)))
